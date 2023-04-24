3574d157bf31194bf2ebcc18bef1065c38024512
= <=buidangcong668899@gmail.com>
2023-04-25 06:40:36 +0700

    init project

---
+++ src/bai_1/Bai_1.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bai_1;
+
+import java.awt.BorderLayout;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=(a==1)? 20:30;
+        System.out.println("Value of b is :" + b);
+        b=(a==10)? 20:30;
+        System.out.println("Value of b is :" + b);
+   
+    }
+    
+}
